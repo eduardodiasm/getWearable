698495379157e6f92bf20abe47fa577c4efca58b
leitegs <leitedasilvag@gmail.com>
2021-03-09 16:39:36 -0300

    Separando a newsletter

---
+++ front/components/newsletter.php
@@ -0,0 +1,18 @@
+<section class="ftco-section ftco-no-pt ftco-no-pb py-5 bg-light">
+  <div class="container py-4">
+    <div class="row d-flex justify-content-center py-5">
+      <div class="col-md-6">
+        <h2 style="font-size: 22px;" class="mb-0">Assine a nossa Newsletter</h2>
+        <span>Insira seu email para ficar por dentro das ofertas</span>
+      </div>
+      <div class="col-md-6 d-flex align-items-center">
+        <form action="#" class="subscribe-form">
+          <div class="form-group d-flex">
+            <input type="text" class="form-control" placeholder="Insira seu email">
+            <input type="submit" value="Inscreva-se" class="submit px-3">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
