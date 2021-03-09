116c93548768f713a24c03ce9987eae394f45a3b
leitegs <leitedasilvag@gmail.com>
2021-03-09 16:32:18 -0300

    Fazendo a reutilização de código

---
+++ front/components/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+  <div class="container">
+    <a class="navbar-brand" href="index.html">getWearable</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="oi oi-menu"></span> Menu
+    </button>
+
+    <div class="collapse navbar-collapse" id="ftco-nav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active"><a href="./index.html" class="nav-link">Inicio</a></li>
+        <li class="nav-item active"><a href="./shop.html" class="nav-link">Loja</a></li>
+        <li class="nav-item active"><a href="./login.html" class="nav-link">Login</a></li>
+
+        <!-- Verificar se o doido ta logado -->
+        <li class="nav-item cta cta-colored"><a href="cart.html" class="nav-link"><span
+              class="icon-shopping_cart"></span>[0]</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
