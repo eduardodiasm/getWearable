944597b35c7d7d48abfed3a7b30185ba9158d832
leitegs <leitedasilvag@gmail.com>
2021-03-27 19:25:59 -0300

    refat: incluindo head

--- back/signup.php
+++ back/signup.php
@@ -1,3 +1,32 @@
+<head>
+    <title>getWearable</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="../front/csscss/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="../front/css/animate.css">
+    
+    <link rel="stylesheet" href="../front/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../front/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../front/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="../front/css/aos.css">
+
+    <link rel="stylesheet" href="../front/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="../front/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="../front/css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="../front/css/flaticon.css">
+    <link rel="stylesheet" href="../front/css/icomoon.css">
+    <link rel="stylesheet" href="../front/css/style.css">
+</head>
+
 <?php
   $name = $_POST['name'];
   $email = $_POST['email'];
