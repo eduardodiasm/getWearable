e83440b73cf8220a5e9b3f535dfa69fdd76aea44
Lucas Kaczynski Primon <lucaskaczyn@gmail.com>
2021-04-06 22:06:45 -0300

    Adicionado migrations do banco

--- scripts-criacao-banco/migrations/20210407004606-user.js
+++ scripts-criacao-banco/migrations/20210407004606-user.js
@@ -3,7 +3,23 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('user', { 
-      id: Sequelize.INTEGER 
+      id: Sequelize.INTEGER ,
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      photo: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
     });
   },
 
