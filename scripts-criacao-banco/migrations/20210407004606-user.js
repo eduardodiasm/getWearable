e88a410b5345522e11f5d4366c4c03b01c8c3e09
leitegs <leitedasilvag@gmail.com>
2021-04-06 22:16:30 -0300

    feat: Implementando migration de usuario

--- scripts-criacao-banco/migrations/20210407004606-user.js
+++ scripts-criacao-banco/migrations/20210407004606-user.js
@@ -3,7 +3,34 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('user', { 
-      id: Sequelize.INTEGER 
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        field: 'id',
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'name',
+      },
+      
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'email',
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'password',
+      },
+      photo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'photo',
+      },
     });
   },
 
