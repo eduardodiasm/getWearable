55b742c8814f0fa053fa3d27b0f36649811cc9a3
Cadu <carloseduardodiasm@gmail.com>
2021-04-10 19:51:27 -0300

    -allow-empty

---
+++ back/user/controller/get.php
@@ -0,0 +1,11 @@
+<?php
+
+  include (dirname(__DIR__).'../../database/connection.php');
+
+  function buscarUsuarios () {
+    $query = "SELECT * FROM user";
+    $query_result = mysqli_query($GLOBALS['connect'], $query);
+    return $query_result;
+  }
+
+?>
