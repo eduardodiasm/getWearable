256fc3d714344c16673431c401a6f7953a0cf7cb
Lucas Kaczynski Primon <lucaskaczyn@gmail.com>
2021-04-14 20:44:07 -0300

    Criada a inserção de imagem dos produtos

--- front/products.php
+++ front/products.php
@@ -51,7 +51,7 @@
 			<div class="row justify-content-center">
 				<div class="col-md-10 mb-5 text-center">
 					<ul class="product-category">
-						<li><a href="./add-product.php" class="active">Adicionar Produto</a></li>
+						<li><a href="./admin/add-product.php" class="active">Adicionar Produto</a></li>
 					</ul>
 				</div>
 			</div>
