b29fef1cda74a17cfad94260d7c19374f9fe5ff1
leitegs <leitedasilvag@gmail.com>
2021-05-07 22:22:29 -0300

    feat: Implementando botão para adicionar item ao carrinho

--- front/product-single.php
+++ front/product-single.php
@@ -34,6 +34,10 @@
 			$idProduto = $_GET["id"];
 			require ('../back/product/controller/buscarPeloId.php');
 			$produto = mysqli_fetch_array(buscarPeloId($idProduto));
+
+			if(!isset($_SESSION['cart'])) {
+				$_SESSION['cart'] = array();	
+			}
 		?>
 
     <div class="hero-wrap hero-bread" style="background-image: url('https://atriumhealth.org/dailydose/-/media/daily-dose-blog/featured-images/hw-fitness-tracker_featured.jpg?h=1080&la=en&w=1920&hash=054879BF8652A5A951D02ABC5BAF92B2C84AA142');">
@@ -49,58 +53,52 @@
 
     <section class="ftco-section">
     	<div class="container">
-    		<div class="row">
-    			<div class="col-lg-6 mb-5 ftco-animate">
-    				<a href="images/product-1.jpg" class="image-popup"><img src="../back/image/products/<?php echo $produto['principal_photo']; ?>" class="img-fluid" alt="Colorlib Template"></a>
-    			</div>
-    			<div class="col-lg-6 product-details pl-md-5 ftco-animate">
-    				<h3><?php echo $produto['name']; ?></h3>
-    				<div class="rating d-flex">
-							<p class="text-left mr-4">
-								<a href="#" class="mr-2">5.0</a>
-								<a href="#"><span class="ion-ios-star-outline"></span></a>
-								<a href="#"><span class="ion-ios-star-outline"></span></a>
-								<a href="#"><span class="ion-ios-star-outline"></span></a>
-								<a href="#"><span class="ion-ios-star-outline"></span></a>
-								<a href="#"><span class="ion-ios-star-outline"></span></a>
-							</p>
-							<!-- <p class="text-left mr-4">
-								<a href="#" class="mr-2" style="color: #000;">100 <span style="color: #bbb;">Rating</span></a>
-							</p>
-							<p class="text-left">
-								<a href="#" class="mr-2" style="color: #000;">500 <span style="color: #bbb;">Sold</span></a>
-							</p> -->
+				<form action="../back/cart/controller/add_product_in_cart.php?adicionar=<?php echo $produto['id']?>" id="form_send" method="POST">
+					<div class="row">
+						<div class="col-lg-6 mb-5 ftco-animate">
+							<a href="images/product-1.jpg" class="image-popup"><img src="../back/image/products/<?php echo $produto['principal_photo']; ?>" class="img-fluid" alt="Colorlib Template"></a>
 						</div>
-    				<p class="price"><span>R$ <?php echo $produto['price']; ?>.00 </span></p>
-    				<p>
-							<?php echo $produto['description']; ?>
-						</p>
-						<div class="row mt-4">
-							<div class="col-md-6">
-								<h6>Quantidade desejada</h6>
+						<div class="col-lg-6 product-details pl-md-5 ftco-animate">
+							<h3><?php echo $produto['name']; ?></h3>
+							<hr>
+							<p class="price"><span>R$ <?php echo $produto['price']; ?>.00 </span></p>
+							<p>
+								<?php echo $produto['description']; ?>
+							</p>
+							<div class="row mt-4">
+								<div class="col-md-6">
+									<h6>Quantidade desejada</h6>
+								</div>
+								<div class="w-100"></div>
+								<div class="input-group col-md-6 d-flex mb-3">
+									<span class="input-group-btn mr-2">
+											<button type="button" class="quantity-left-minus btn"  data-type="minus" data-field="">
+											<i class="ion-ios-remove"></i>
+											</button>
+										</span>
+									<input type="text" id="quantity" name="quantity" class="form-control input-number" value="1" min="1" max="100">
+									<span class="input-group-btn ml-2">
+											<button type="button" class="quantity-right-plus btn" data-type="plus" data-field="">
+												<i class="ion-ios-add"></i>
+										</button>
+									</span>
+								</div>
+								<div class="w-100"></div>
+								<div class="col-md-12">
+										<script>
+												var val = document.getElementById('quantity').value
+												function link() {
+													return val;
+												}
+										</script>
+										
+									<p style="color: #000;">Quantidade disponível: <?php echo $produto['quantity']; ?></p>
+								</div>
 							</div>
-							<div class="w-100"></div>
-							<div class="input-group col-md-6 d-flex mb-3">
-	             	<span class="input-group-btn mr-2">
-	                	<button type="button" class="quantity-left-minus btn"  data-type="minus" data-field="">
-	                   <i class="ion-ios-remove"></i>
-	                	</button>
-	            		</span>
-	             	<input type="text" id="quantity" name="quantity" class="form-control input-number" value="1" min="1" max="100">
-	             	<span class="input-group-btn ml-2">
-	                	<button type="button" class="quantity-right-plus btn" data-type="plus" data-field="">
-	                     <i class="ion-ios-add"></i>
-	                 </button>
-	             	</span>
-	          	</div>
-	          	<div class="w-100"></div>
-	          	<div class="col-md-12">
-	          		<p style="color: #000;">Quantidade disponível: <?php echo $produto['quantity']; ?></p>
-	          	</div>
-          	</div>
-          	<p><a href="cart.php" class="btn btn-black py-3 px-5">Adicionar ao carrinho</a></p>
-    			</div>
-    		</div>
+							 <a class="btn btn-black py-3 px-5" onClick="document.getElementById('form_send').submit();">Adicionar ao carrinho</a>	
+						</div>
+					</div>
+				</form>
     	</div>
     </section>
 
