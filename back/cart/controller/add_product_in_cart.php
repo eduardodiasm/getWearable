55bc689ec84590d2bcc0918fac57929dbb3e4966
leitegs <leitedasilvag@gmail.com>
2021-05-07 22:22:48 -0300

    refat: pegando a quantidade que o usuario pretende comprar

--- back/cart/controller/add_product_in_cart.php
+++ back/cart/controller/add_product_in_cart.php
@@ -2,13 +2,24 @@
   session_start();
 
   $productId = intval($_GET['adicionar']);
+  $quantidade = $_POST['quantity'];
 
   if(!isset($_SESSION['cart'][$productId]['quantidade'])) {
-    $_SESSION['cart'][$productId]['quantidade'] = 1; 
+    if(!isset($quantidade)) {
+      $_SESSION['cart'][$productId]['quantidade'] = 1; 
+    }
+    else {
+      $_SESSION['cart'][$productId]['quantidade'] = $quantidade; 
+    }
   }
 
   else {
-    $_SESSION['cart'][$productId]['quantidade']++; 
+    if(!isset($quantidade)) {
+      $_SESSION['cart'][$productId]['quantidade']++;  
+    }
+    else {
+      $_SESSION['cart'][$productId]['quantidade'] = $quantidade; 
+    }
   }
 
   header("Location: ../../../front/shop.php");
