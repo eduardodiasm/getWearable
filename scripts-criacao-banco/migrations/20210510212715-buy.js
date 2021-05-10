9b23a769bfb7a222967a7e97987e51fb3559a7d9
leitegs <leitedasilvag@gmail.com>
2021-05-10 18:44:36 -0300

    feat: Implementando migration de compra

---
+++ scripts-criacao-banco/migrations/20210510212715-buy.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.createTable('buy', { 
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        field: 'id',
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'address',
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'phone',
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'city',
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'state',
+      },
+      method_payment: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'method_payment',
+      }
+     });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('buy');
+  }
+};
