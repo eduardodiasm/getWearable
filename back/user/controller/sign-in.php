80f1ddb7e8bf84ac4784b5e3e80ed8d7591f9e3d
leitegs <leitedasilvag@gmail.com>
2021-05-11 15:49:12 -0300

    refat: ajustando login que estava sem verificação

--- back/user/controller/sign-in.php
+++ back/user/controller/sign-in.php
@@ -1,38 +1,39 @@
 <?php
 
-  include (dirname(__DIR__).'../../database/connection.php');
-
-  function buscarUsuarioPorEmailSenha ($email, $password) {
-    $query = "SELECT * FROM user WHERE email = '$email' and password = '$password'";
-    $query_result = mysqli_query($GLOBALS['connect'], $query);
-    return $query_result;
-  }
-
-  function startarSessao ($email, $idUsuario) {
-    session_start();
-    $_SESSION["email"] = $email;
-    $_SESSION["idUsuario"] = $idUsuario;
-  }
+  include '../../database/connection.php';
 
   $email = $_POST['email'];
   $password = $_POST['password'];
-  $resultado_busca = buscarUsuarioPorEmailSenha($email, $password);
-  $encontrou_usuario = mysqli_num_rows($resultado_busca);
-  
-  if (!$encontrou_usuario) {
+
+  $query = "SELECT (email, id) FROM user WHERE email = '$email' and password = '$password'";
+
+  $query_result = mysqli_query($connect, $query);
+
+  $user = mysqli_num_rows($query_result);
+  if($user <= 0) {
     echo("
       <script language='javascript' type='text/javascript'>
-        alert('Todos os campos precisam ser preenchidos');
-        window.location.href='../front/signin.php';
+        alert('login e/ou senha incorretos');
       </script>
     ");
+
+    header("refresh: 0; url='../../../front/signin.php'");
+  } 
+  else {
+    function startarSessao ($email, $idUsuario) {
+      session_start();
+      $_SESSION["email"] = $email;
+      $_SESSION["idUsuario"] = $idUsuario;
+    }
+
+    $idUsuario = mysqli_fetch_array($query_result)[0];
+  
+    startarSessao($email, $idUsuario);
+  
+    header("Location:  ../../../front/shop.php");
+    
+    exit;
   }
 
-  $idUsuario = mysqli_fetch_array($resultado_busca)[0];
-
-  startarSessao($email, $idUsuario);
-
-  header("Location:  ../../../front/shop.php");
-  exit;
 
 ?>
