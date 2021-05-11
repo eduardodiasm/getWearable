5cfb1ba5142188d0510220e834cc62d7095d0e09
leitegs <leitedasilvag@gmail.com>
2021-05-11 16:05:13 -0300

    refat: ajustando o login que tava bugado

--- back/user/controller/sign-in.php
+++ back/user/controller/sign-in.php
@@ -5,12 +5,11 @@
   $email = $_POST['email'];
   $password = $_POST['password'];
 
-  $query = "SELECT (email, id) FROM user WHERE email = '$email' and password = '$password'";
+  $query = "SELECT * FROM user WHERE email = '$email' and password = '$password'";
 
   $query_result = mysqli_query($connect, $query);
 
-  $user = mysqli_num_rows($query_result);
-  if($user <= 0) {
+  if(mysqli_num_rows($query_result) <= 0) {
     echo("
       <script language='javascript' type='text/javascript'>
         alert('login e/ou senha incorretos');
@@ -18,21 +17,20 @@
     ");
 
     header("refresh: 0; url='../../../front/signin.php'");
-  } 
-  else {
+  } else {
     function startarSessao ($email, $idUsuario) {
       session_start();
       $_SESSION["email"] = $email;
       $_SESSION["idUsuario"] = $idUsuario;
     }
-
+  
     $idUsuario = mysqli_fetch_array($query_result)[0];
   
     startarSessao($email, $idUsuario);
   
     header("Location:  ../../../front/shop.php");
-    
     exit;
+
   }
 
 
