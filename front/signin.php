b4507d46959d9954bfe745d02a27dea00b496e75
leitegs <leitedasilvag@gmail.com>
2021-05-11 15:49:28 -0300

    feat: adicionando refresh

--- front/signin.php
+++ front/signin.php
@@ -5,6 +5,7 @@
   <title>getWearable</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="refresh" content="0; />
 
   <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap"
     rel="stylesheet">
